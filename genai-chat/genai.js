6a84242c813d282722ba00996b1e96fef263cbca
AguzzTN54 <gusagusnanang@gmail.com>
2024-07-19 21:15:22 +0700

    SingleQuote

--- genai-chat/genai.js
+++ genai-chat/genai.js
@@ -25,7 +25,7 @@
 
 // Messages List, copy from Lab!
 const messages = [
-    "Hello",
+    'Hello',
     'What is the current time in "topic"?',
     'What can you tell me about the "item"?',
     'Can you list three important facts about "topic" ?',
@@ -35,14 +35,14 @@ const messages = [
     'What teams are playing in "topic"',
 ];
 
-let lab_persona = "";
-let service_url = "";
-let lab_topic = "";
+let lab_persona = '';
+let service_url = '';
+let lab_topic = '';
 
-const cors = "https://api.wishsimulator.app/gcsb?u="; // CORS Anywhere, Replace this if necessary.
-const corePath = "/main.dart.js";
-const quizPath = "/assets/config/tasks.json";
-const personaPath = "/assets/config/persona.json";
+const cors = 'https://api.wishsimulator.app/gcsb?u='; // CORS Anywhere, Replace this if necessary.
+const corePath = '/main.dart.js';
+const quizPath = '/assets/config/tasks.json';
+const personaPath = '/assets/config/persona.json';
 
 /**
  * CHATTING TASK
@@ -50,8 +50,8 @@ const personaPath = "/assets/config/persona.json";
 const getChatAPIHost = async () => {
     const data = await fetch(cors + service_url + corePath);
     const txt = await data.text();
-    const [, bk] = txt.split("arcade-api-");
-    const [id] = bk.split(".run.app");
+    const [, bk] = txt.split('arcade-api-');
+    const [id] = bk.split('.run.app');
     const host = `https://arcade-api-${id}.run.app`;
     return host;
 };
@@ -63,20 +63,17 @@ const chatPersona = async (personaURL) => {
      */
     for (const msg of messages) {
         const topic = msg.replace(/"topic"/gi, lab_topic);
-        const talk = await fetch(personaURL + "&message=" + topic);
+        const talk = await fetch(personaURL + '&message=' + topic);
         const { msg: response } = await talk.json();
         console.group(topic);
         console.log(response);
         console.groupEnd(msg);
-        console.log("");
+        console.log('');
     }
 };
 
 const chatTask = async () => {
-    console.log(
-        "%cFetching ChatBot.. Please Wait!",
-        "color: #4285f4; font-weight:bold;"
-    );
+    console.log('%cFetching ChatBot.. Please Wait!', 'color: #4285f4; font-weight:bold;');
     const data = await fetch(cors + service_url + personaPath);
     const { persona } = await data.json();
     const usedPersona = persona.find(({ name }) => name === lab_persona);
@@ -96,7 +93,7 @@ const quizTask = async () => {
     const targetURL = `https://${uri}${endpoint}?message=arcade`;
     const finalize = await fetch(targetURL);
     const response = await finalize.text();
-    console.group("%cQuiz Task Done✔✔✔", "color: #34a853;");
+    console.group('%c Quiz Task Done✔✔✔', 'color: #34a853;');
     console.log(response);
     console.groupEnd();
     return response;
@@ -106,34 +103,28 @@ const quizTask = async () => {
  * LAB CONTROLLER
  */
 const checkLab = async (labInstanceId) => {
-    console.log(
-        "%c Almost Done.. Verifying..",
-        "color:#00aaff; font-weight:800;"
-    );
+    console.log('%c Almost Done.. Verifying..', 'color:#00aaff; font-weight:800;');
 
-    const qwiklab = "https://www.cloudskillsboost.google";
+    const qwiklab = 'https://www.cloudskillsboost.google';
     const stepURL = `${qwiklab}/assessments/run_step.json?id=${labInstanceId}&step=1&u=${Math.random()}`;
     const data = await fetch(stepURL);
     const { percent_complete } = await data.json();
     const isComplete = percent_complete === 100;
 
     if (!isComplete) return checkLab(labInstanceId);
-    console.log(
-        "%c LAB ENDED !",
-        "color:green; font-weight:800; font-size: 200%"
-    );
+    console.log('%c LAB ENDED !', 'color:green; font-weight:800; font-size: 200%');
 
     // Check Progress UI for better satisfaction :D.
-    const trackerPanel = document.querySelector("ql-activity-tracking");
-    const checkButton = trackerPanel.shadowRoot.querySelector("ql-button");
-    const btn = checkButton.shadowRoot.querySelector("button");
+    const trackerPanel = document.querySelector('ql-activity-tracking');
+    const checkButton = trackerPanel.shadowRoot.querySelector('ql-button');
+    const btn = checkButton.shadowRoot.querySelector('button');
     btn.click();
     endLab();
 };
 
 const endLab = async () => {
-    const finalize = document.querySelector("#js-are-you-sure-button");
-    const finalizeButton = finalize.shadowRoot.querySelector("button");
+    const finalize = document.querySelector('#js-are-you-sure-button');
+    const finalizeButton = finalize.shadowRoot.querySelector('button');
     finalizeButton.click();
 };
 
@@ -147,19 +138,13 @@ const taskCheater = async (resource) => {
         const { startup_script = {} } = resourceData?.project_0;
 
         if (!startup_script.service_url) {
-            console.log(
-                "%c PREPARING RESOURCES, Please Wait!",
-                "color: orange; font-weight:bold"
-            );
+            console.log('%c PREPARING RESOURCES, Please Wait!', 'color: orange; font-weight:bold');
             return;
         }
 
         // Don't run function if already executed
         if (isProcessed) return;
-        console.log(
-            "%c LAB STARTED !",
-            "color:#d93025; font-weight:800; font-size: 200%"
-        );
+        console.log('%c LAB STARTED !', 'color:#d93025; font-weight:800; font-size: 200%');
         console.table(startup_script);
         isProcessed = true;
 
@@ -177,7 +162,7 @@ window.fetch = async (...args) => {
     const response = await originalFetch(resource, config);
     const data = response.clone();
 
-    if (resource.includes("focuses/show")) {
+    if (resource.includes('focuses/show')) {
         taskCheater(data);
     }
     return response;
