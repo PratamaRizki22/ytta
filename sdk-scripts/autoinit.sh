48b0c2a3c1b849eced8d77312f089b9cd3f72e92
AguzzTN54 <gusagusnanang@gmail.com>
2024-08-10 22:59:06 +0700

    Expect to sign in

--- sdk-scripts/autoinit.sh
+++ sdk-scripts/autoinit.sh
@@ -7,35 +7,34 @@ spawn gcloud init --skip-diagnostics
 expect "Re-initialize this configuration";
 send -- "1\r"
 
-expect "Would you like to log in";
+expect "in to continue";
 send -- "Y\r"
 
 expect {
 	# if auth key needed
 	-re "enter the verification code provided in your browser"
 	{
-		set key ""
-		while {$key == ""} {
+		set AUTH_KEY ""
+		while {$AUTH_KEY == ""} {
 			set k [open "./tmp/login_key.txt" r]
-			set key	[read $k]
+			set AUTH_KEY [read $k]
 			close $k
 
-			if {$key != ""} { send -- "$key\r"; exp_continue }
+			if {$AUTH_KEY != ""} { send -- "$AUTH_KEY\r"; exp_continue }
 			sleep 1
 		}
 	}
 
-	# Send Project ID
-	-re {\d+] Enter a project ID}
+	-re {\d+] Enter a project ID} 			# Select Project ID
+	# -re {\d+] qwiklabs-resources}			# Use Qwiklab Resource for faster init
 	{ set num $expect_out(0,string); send -- "[lindex [split $num "]"] 0]\r" }
 }
 
 set f [open "./tmp/project_id.txt" r]
-set extracted_file [split [read $f] "\n"]
-set project_id [lindex $extracted_file 0]
+set PROJECT_ID [read $f]
 close $f
 
 expect  "project ID you would like to use"; 
-send -- "$project_id\r"
+send -- "$PROJECT_ID\r"
  
 expect eof
